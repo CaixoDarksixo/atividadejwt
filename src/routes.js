36ba96bcde9c862e322a52b464cae73b0ae62190
CaixoDarksixo <jpnossol@gmail.com>
2024-11-18 22:22:17 -0300

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { login, protectedContent, whisperContent, getGit } = require("./controllers/authController");
+
+const router = express.Router();
+
+// Rota pública
+router.get("/", (request, response) => {
+  response.json({ message: "Endpoint que não exige autenticação!" });
+});
+
+// Rota de login
+router.post("/login", login);
+
+// Rota protegida
+router.get("/protected", protectedContent);
+
+// Sussurro
+router.get("/whisper", whisperContent);
+
+// github
+router.get("/git", getGit);
+router.get("/github", getGit);
+
+module.exports = router;
